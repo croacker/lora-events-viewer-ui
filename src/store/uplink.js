86de5359d13423e316ae81166ab58043fc6cb814
croacker <croacker@mail.ru>
2019-09-02 00:18:53 +0800

    work

---
+++ src/store/uplink.js
@@ -0,0 +1,23 @@
+const state = {
+    uplinks: [],
+    uplink: null
+};
+
+const mutations = {
+
+};
+
+const actions = {
+
+};
+
+const getters = {
+
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
