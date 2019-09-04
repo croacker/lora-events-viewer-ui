8b5b6c7a6013c9918cb216db2153d3b5e71913d2
croacker <croacker@mail.ru>
2019-09-04 11:45:44 +0800

    work

--- src/components/UplinkTable.vue
+++ src/components/UplinkTable.vue
@@ -1,85 +1,85 @@
 <template>
   <div class="full-width-data-table">
-  <v-row>
-    <v-col cols="12" sm="6" md="2">
-      <v-menu
-              ref="menuDateFrom"
-              v-model="menuDateFrom"
-              :close-on-content-click="false"
-              transition="scale-transition"
-              offset-y
-              full-width
-              min-width="290px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-text-field
-                  v-model="filter.dateFrom"
-                  label="Дата с"
-                  prepend-icon="mdi-calendar"
-                  readonly
-                  v-on="on"
-          ></v-text-field>
-        </template>
-        <v-date-picker v-model="filter.dateFrom" @input="filterDateFromChange"></v-date-picker>
-      </v-menu>
-    </v-col>
-    <div class="flex-grow-1"></div>
-    <v-col cols="12" sm="6" md="2">
-      <v-menu
-              ref="menuDateFrom"
-              v-model="menuDateTo"
-              :close-on-content-click="false"
-              transition="scale-transition"
-              offset-y
-              full-width
-              min-width="290px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-text-field
-                  v-model="filter.dateTo"
-                  label="по"
-                  prepend-icon="mdi-calendar"
-                  readonly
-                  v-on="on"
-          ></v-text-field>
-        </template>
-        <v-date-picker v-model="filter.dateTo" @input="filterDateToChange"></v-date-picker>
-      </v-menu>
-    </v-col>
-    <v-col cols="12" sm="6" md="4">
-      <v-text-field
-              v-model="filter.deviceName"
-              label="Фильтр deviceName"
-              prepend-icon="mdi-filter"
-              @input="filterChange"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" sm="6" md="4">
-      <v-text-field
-              v-model="filter.applicationName"
-              label="Фильтр applicationName"
-              prepend-icon="mdi-filter"
-              @input="filterChange"
-      ></v-text-field>
-    </v-col>
-    <div class="flex-grow-1"></div>
-  </v-row>
-  <v-data-table
-    dense
-    :headers="headers"
-    :items="eventItems"
-    :options="options"
-    :server-items-length="serverItemsLength"
-    item-key="id"
-    class="elevation-1 full-width-data-table"
-    @update:page="pageChange"
-    @update:items-per-page="itemsPerPageChange"
-    fill-height
-  >
-    <!-- <template v-slot:item.dataBase64="{ item }">
+    <v-row>
+      <v-col cols="12" sm="6" md="2">
+        <v-menu
+          ref="menuDateFrom"
+          v-model="menuDateFrom"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="filter.dateFrom"
+              label="Дата с"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="filter.dateFrom" @input="filterDateFromChange"></v-date-picker>
+        </v-menu>
+      </v-col>
+      <div class="flex-grow-1"></div>
+      <v-col cols="12" sm="6" md="2">
+        <v-menu
+          ref="menuDateFrom"
+          v-model="menuDateTo"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="filter.dateTo"
+              label="по"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="filter.dateTo" @input="filterDateToChange"></v-date-picker>
+        </v-menu>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field
+          v-model="filter.deviceName"
+          label="Фильтр deviceName"
+          prepend-icon="mdi-filter"
+          @input="filterChange"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field
+          v-model="filter.applicationName"
+          label="Фильтр applicationName"
+          prepend-icon="mdi-filter"
+          @input="filterChange"
+        ></v-text-field>
+      </v-col>
+      <div class="flex-grow-1"></div>
+    </v-row>
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="eventItems"
+      :options="options"
+      :server-items-length="serverItemsLength"
+      item-key="id"
+      class="elevation-1 full-width-data-table"
+      @update:page="pageChange"
+      @update:items-per-page="itemsPerPageChange"
+      fill-height
+    >
+      <!-- <template v-slot:item.dataBase64="{ item }">
       <v-icon small class="mr-2" @click="showPayload(item)">edit</v-icon>
-    </template>-->
-  </v-data-table>
+      </template>-->
+    </v-data-table>
   </div>
 </template>
 
@@ -109,7 +109,7 @@ export default {
       { text: "receivedAt", value: "receivedAt" },
       { text: "devEui", value: "devEuiHex" },
       { text: "deviceName", value: "deviceName" },
-     {
+      {
         text: "applicationName",
         value: "applicationName",
         width: "100px",
@@ -126,9 +126,9 @@ export default {
     menuDateFrom: false,
     dateTo: new Date().toISOString().substr(0, 10),
     menuDateTo: false,
-    filterDeviceName: '',
-    filterApplicationName:'',
-    filter: new UplinkFilter(),
+    filterDeviceName: "",
+    filterApplicationName: "",
+    filter: new UplinkFilter()
   }),
   created: function() {
     this.getTotalCount();
@@ -142,21 +142,21 @@ export default {
       this.options.itemsPerPage = itemsPerPage;
       this.getItems();
     },
-    filterDateFromChange(){
+    filterDateFromChange() {
       this.menuDateFrom = false;
-      this.filterChange()
+      this.filterChange();
     },
-    filterDateToChange(){
+    filterDateToChange() {
       this.menuDateTo = false;
-      this.filterChange()
+      this.filterChange();
     },
-    filterChange(){
+    filterChange() {
       this.getTotalCount();
       this.getItems();
     },
-    on(){},
+    on() {},
     getTotalCount() {
-      const url = `${GET_URL}/count`;
+      const url = `${GET_URL}/count?${this.filter.buildWhere()}`;
       axios.get(url).then(response => {
         this.serverItemsLength = response.data.count;
       });
@@ -164,7 +164,7 @@ export default {
     getItems() {
       const limit = this.options.itemsPerPage;
       const offset = limit * (this.options.page - 1);
-      const url = `${GET_URL}?filter[limit]=${limit}&filter[offset]=${offset}&filter[order]=receivedAt%20DESC${this.filter.build()}`;
+      const url = `${GET_URL}?filter[limit]=${limit}&filter[offset]=${offset}&filter[order]=receivedAt%20DESC&${this.filter.buildFilter()}`;
       axios.get(url).then(response => {
         response.data.map(function(item) {
           item.devEuiHex = DataService.byteToHex(item.devEui.data);
@@ -178,47 +178,79 @@ export default {
   }
 };
 
-class UplinkFilter{
-  constructor(){
-    this.deviceName = '';
-    this.applicationName = '';
-    this.dateFrom = this.previousMonth().toISOString().substr(0, 10);
+class UplinkFilter {
+  constructor() {
+    this.deviceName = "";
+    this.applicationName = "";
+    this.dateFrom = this.previousWeek()
+      .toISOString()
+      .substr(0, 10);
     this.dateTo = new Date().toISOString().substr(0, 10);
   }
 
-  build(){
-    return `${this.getBetween()}${this.getDeviceName()}${this.getApplicationName()}`
+  buildFilter() {
+    return `${this.getFilterBetween()}${this.getFilterDeviceName()}${this.getFilterApplicationName()}`;
   }
 
-  getBetween(){
-    const from = new Date(this.dateFrom).toISOString()
-    const to = new Date(`${this.dateTo}T23:59:59.999Z`).toISOString()
-    return `&filter[where][receivedAt][between][0]=${from}&filter[where][receivedAt][between][1]=${to}`
+  buildWhere() {
+    return `${this.getWhereBetween()}${this.getWhereDeviceName()}${this.getWhereApplicationName()}`;
   }
 
-  getDeviceName(){
-    let filter = '';
-    if(this.deviceName){
-      filter = `&filter[where][deviceName][like]=%${this.deviceName}%`
+  getFilterBetween() {
+    const from = new Date(this.dateFrom).toISOString();
+    const to = new Date(`${this.dateTo}T23:59:59.999Z`).toISOString();
+    return `filter[where][receivedAt][between][0]=${from}&filter[where][receivedAt][between][1]=${to}`;
+  }
+
+  getWhereBetween() {
+    const from = new Date(this.dateFrom).toISOString();
+    const to = new Date(`${this.dateTo}T23:59:59.999Z`).toISOString();
+    return `where[receivedAt][between][0]=${from}&where[receivedAt][between][1]=${to}`;
+  }
+
+  getFilterDeviceName() {
+    let filter = "";
+    if (this.deviceName) {
+      filter = `&filter[where][deviceName][like]=%${this.deviceName}%`;
     }
-    return filter
+    return filter;
   }
 
-  getApplicationName(){
-    let filter = '';
-    if(this.applicationName){
-      filter = `&filter[where][applicationName][like]=%${this.applicationName}%`
+  getWhereDeviceName() {
+    let where = "";
+    if (this.deviceName) {
+      where = `&where[deviceName][like]=%${this.deviceName}%`;
     }
-    return filter
+    return where;
   }
 
-  previousMonth(){
+  getFilterApplicationName() {
+    let filter = "";
+    if (this.applicationName) {
+      filter = `&filter[where][applicationName][like]=%${this.applicationName}%`;
+    }
+    return filter;
+  }
+
+  getWhereApplicationName() {
+    let where = "";
+    if (this.applicationName) {
+      where = `&where[applicationName][like]=%${this.applicationName}%`;
+    }
+    return where;
+  }
+
+  previousMonth() {
     const now = new Date();
     return now.getMonth() === 0
-            ? new Date(now.getFullYear() - 1, 11, now.getDate())
-            : new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+      ? new Date(now.getFullYear() - 1, 11, now.getDate())
+      : new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
   }
 
+  previousWeek() {
+    const today = new Date();
+    return new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+  }
 }
 </script>
 
