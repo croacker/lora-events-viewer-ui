a3a5bbb5d236e5fd410fdd1058e2cda7d93924fc
croacker <croacker@mail.ru>
2019-09-18 22:34:41 +0800

    add store

--- src/store.js
+++ src/store.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import VuexPersistence from 'vuex-persist'
 import app from "./store/app"
-import uplink from "./store/uplink"
+import uplink from "./store/uplink-store"
+import ack from "./store/ack-store"
+import error from "./store/error-store"
+import join from "./store/join-store"
+import location from "./store/location-store"
+import status from "./store/status-store"
 import rxInfoDialog from "./store/rx-info-dialog"
 
 Vue.use(Vuex)
@@ -24,6 +29,11 @@ export default new Vuex.Store({
   },
   modules:{
     app,
+    ack,
+    error,
+    join,
+    location,
+    status,
     uplink,
     rxInfoDialog
   },
